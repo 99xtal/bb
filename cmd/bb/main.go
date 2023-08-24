d7858765f695fb0e8fb1474b8c28d0ed3231b689
jrybarczyk <jrybarczyk@exclusiveresorts.com>
2023-08-23 23:35:57 -0400

    feat: add Cobra command

--- cmd/bb/main.go
+++ cmd/bb/main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
+  "context"
   "fmt"
+  "os"
+  
+  "github.com/99xtal/bb/pkg/cmd/root"
 )
 
+type exitCode int
+
+const (
+	exitOK     exitCode = 0
+	exitError  exitCode = 1
+	exitCancel exitCode = 2
+	exitAuth   exitCode = 4
+)
+
+func run() exitCode {
+   ctx := context.Background()
+
+  rootCmd, err := root.NewCmdRoot()
+  if err != nil {
+    fmt.Fprintf(os.Stderr, "failed to create root command: %s\n", err)
+    return exitError
+  }
+
+  _, err = rootCmd.ExecuteContextC(ctx)
+  if err != nil {
+    fmt.Fprintf(os.Stderr, "%s\n", err)
+    return exitError
+  }
+  
+  return exitOK
+}
+
 func main() {
-  fmt.Println("Hello, world!");
+  code := run()
+  os.Exit(int(code))
 }
